2717899b566cb3dfb7e861392b95513a08ca67e3
auddus16 <auddus16@naver.com>
2022-03-17 22:58:58 +0900

    220317-당근마켓 데이터 수집

--- CSVtoES_my.py
+++ CSVtoES_my.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+import datetime
 import json
 import urllib
 from xml.etree.ElementTree import fromstring, ElementTree
@@ -6,7 +8,31 @@ from urllib.request import Request, urlopen
 import csv
 import pandas as pd
 
-es = Elasticsearch(['https://buyornot.kb.ap-northeast-2.aws.elastic-cloud.com:9243'])
+es = Elasticsearch(['https://buyornot.kb.ap-northeast-2.aws.elastic-cloud.com:9243'], http_auth=('elastic', 'Orf5PC90BVmMMuVU5cKoTyrs'))
+es.indices.create(
+    index='python_to_elastic',
+    body={
+        "mappings" : {
+            "type1" : {
+                "properties" : {
+                    "field1" : {
+                        "type" : "text"
+                      }
+                  }
+              }
+          }
+      }
+  )
+es.create(
+    index='python_to_elastic',
+    doc_type='type1',
+    body= {
+        "doc" : {
+            "field1" : "value2"
+         }
+     },
+     id = 'abc111'
+)
 
 docs = []
 j = 0
@@ -15,12 +41,23 @@ j = 0
 #     pass
 # else:
 #     es.indices.create(index='daangn_index')
-file= pd.read_excel('당근마켓_0313.xlsx')
-file.to_csv('당근마켓_0313.csv')
+# file= pd.read_excel('당근마켓_220316_1200.xlsx')
+# file.to_csv('당근마켓_220316_1200.csv', index=False)
 
-# with open('당근마켓_log1.csv', 'rt', encoding='UTF8', errors='ignore') as se:
+# with open('당근마켓_220316_1200.csv', 'r', encoding='UTF8', errors='ignore') as se:
 #     reader = csv.reader(se)
-#     helpers.bulk(es, reader, index='carrot_market')
+#     doc = {
+#         "_index": "carrot_market",
+#         "_type" : "aaa",
+#         "_id" : "aaa",
+#         "_source": {
+#             "datalink" : "당근마켓",
+#             "name" : "애플"
+#         }
+#     }
+#     docs.append(doc)
+#     helpers.bulk(es, docs)
+
 
 
 
